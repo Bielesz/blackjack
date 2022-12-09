2911efaf8500ab8b8154f274fb8e450018642cb1
Mariusz Bielesz <bielesz@yahoo.com>
2022-12-09 19:51:32 +0100

    working on blackjack game

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-let count = 0
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+let firstCard = 10;
+let secondCard = 4;
+let cards = [firstCard, secondCard]; // array - ordered list of items
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-function increment() {
-    count += 1
-    countEl.textContent = count
+function startGame() {
+  renderGame();
 }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContenttStr
-    countEl.textContent = 0
-    count = 0
+function renderGame() {
+  // render out firstCard and secondCard
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  // render out ALL the cards we have
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
 }
 
-console.log("Let's count people on the subway!")
+function newCard() {
+  let card = 6;
+  sum += card;
+  renderGame();
+}
